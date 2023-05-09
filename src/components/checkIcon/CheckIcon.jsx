659b27e14a1cf039f24947542d8ed6134283fdf2
Daniel <daniilbondarcov@gmail.com>
2023-05-09 16:22:23 +0300

    Implemented reusable Check Icon component

--- src/components/checkIcon/CheckIcon.jsx
+++ src/components/checkIcon/CheckIcon.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { checkCircleIcon } from '../../utils/consts'
+import './CheckIcon.css'
 
 const CheckIcon = () => {
-    return <img className="checkIcon" src={checkCircleIcon} alt="check icon" />
+    return <img
+        className="checkIcon"
+        src={checkCircleIcon}
+        alt="checked"
+    />
 }
 
 export default CheckIcon
